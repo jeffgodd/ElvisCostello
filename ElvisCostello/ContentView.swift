2a974ed9b466c24b2e808c42216567e06c3d87bd
jeffgodd <jeff@leggierosw.com>
2025-05-18 18:11:47 -0700

    my first try

--- ElvisCostello/ContentView.swift
+++ ElvisCostello/ContentView.swift
@@ -8,12 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message: String = ""
+    @State private var imageView: String = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What's So Funny 'Bout")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.purple)
+            
+            Spacer()
+            
+            Image(systemName: imageView)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300)
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.purple)
+            
+            Spacer()
+            
+            HStack {
+                Button("Peace!") {
+                    message = "Peace"
+                    imageView = "peacesign"
+                }
+
+                Button("Love!") {
+                    message = "Love"
+                    imageView = "heart"
+                }
+                
+                Button("Understanding!") {
+                    message = "Understanding"
+                    imageView = "lightbulb"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.purple)
+            
         }
         .padding()
     }
