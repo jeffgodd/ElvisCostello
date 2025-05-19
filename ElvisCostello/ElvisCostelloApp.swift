cd4ec69122de45d3e9c5081bdcf0515b53daab4b
jeffgodd <jeff@leggierosw.com>
2025-05-18 17:47:15 -0700

    Initial Commit

---
+++ ElvisCostello/ElvisCostelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisCostelloApp.swift
+//  ElvisCostello
+//
+//  Created by Jeff Goddard on 5/18/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisCostelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
